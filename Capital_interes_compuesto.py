d15cad716ad2db8c9f470951c3a40a020e8853df
juanpablo <juan2224649@correo.uis.edu.co>
2022-09-29 15:07:11 +0200

    Se agrega el analisis, diagrama de flujo y construccion del programa

---
+++ Capital_interes_compuesto.py
@@ -0,0 +1,19 @@
+# Ejercicio 26 : Con el capital, en cuantos meses se duplica.
+
+print("-------------------------------")
+print("--CAPITAL--INTERES-COMPUESTO---")
+print("-------------------------------")
+
+# input
+c = int(input("Dijite la capital:"))
+c1 = 0
+i = 0
+
+#process
+while c1<=c*2:
+    c1 = c1 + c * 0.05
+    i = i + 1
+
+#output
+
+print("Los",c,"de capital,se duplica en",i,"meses, teniendo asi ahora",c1)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Capital_interesCompuesto
+# Ejercicio 26 : Con el capital, en cuantos meses se duplica.
+
+# Analisis
+Primer dia : 10.000 ,se suma el 5% de esta
+Segundo dia : 10.500 ,se suma el 5% de esta
+Tercer dia : 11.025 ,se suma el 5% de esta
+n dias : 20.000 
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
